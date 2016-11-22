5bd2b24450d057fc6501e421e54102153698683d
mew <377545660@qq.com>
2016-11-23 00:11:44 +0800

    实现了简单的一步判断，终于搞定DLL啦

--- MyDLL/MainCode.cpp
+++ MyDLL/MainCode.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+//这里是我给的demo，可以不去调用
+void Wy_ProcStep(int x0, int y0, int x1, int y1, int color);
+Step Wy_NextStep();
 
 char Name[64];
 char Introduction[256];
@@ -23,6 +26,64 @@ void InitIntroduction() {
 void ProcStep(int x0, int y0, int x1, int y1, int color)
 {
 	//请在这里自行完成模拟落子、改变棋盘状态的函数
+	Wy_ProcStep(x0, y0, x1, y1, color);
+}
+Step GetNextStep(struct Step requests[], struct Step responses[], int StepCount) {
+	// 初始化棋盘
+	gridInfo[0][0] = gridInfo[6][6] = 1;  //|黑|白|
+	gridInfo[6][0] = gridInfo[0][6] = -1; //|白|黑|
+	//原本这里是读入JSON，这里不需要读入，已知有StepCount个步骤了
+	// 分析自己收到的输入和自己过往的输出，并恢复状态
+	int turnID = StepCount;
+	cout << "Count:" << StepCount << endl;
+	currBotColor = requests[0].X0 < 0 ? 1 : -1; // 第一回合收到坐标是-1, -1，说明我是黑方
+	cout << "Color:" << currBotColor << endl;
+	for (int i = 0; i < turnID; i++)
+	{
+		//cout << "requests[" << i <<"](" << requests[i].X0 << "," << 
+		//	requests[i].Y0 << ")(" << requests[i].X1 << "," << requests[i].Y1  << ")" << endl;
+		if (requests[i].X0 >= 0)
+			ProcStep(requests[i].X0, requests[i].Y0, requests[i].X1,
+				requests[i].Y1, -currBotColor); // 模拟对方落子
+		if (responses[i].X0 >= 0)
+			ProcStep(responses[i].X0, responses[i].Y0, responses[i].X1,
+				responses[i].Y1, currBotColor); // 模拟己方落子%
+	}
+	//下面这段可以向控制台输出棋盘状态
+	/*for (int j = 0; j < 7; j++) {
+		for (int i = 0; i < 7; i++) {
+			if (gridInfo[i][j] == 0) cout << '0';
+			else if (gridInfo[i][j] == 1) cout << '#';
+			else if (gridInfo[i][j] == -1) cout << '.';
+		}
+		cout << endl;
+	}*/
+	// 找出合法落子点并做出决策
+	int startX, startY, resultX, resultY;
+	//进行决策，得出落子坐标，并赋值给上述变量
+	//在这里添加你的代码
+
+	//我的demo，从这里到下一个注释可以DIY
+	Step re = Wy_NextStep();
+	startX = re.X0;
+	startY = re.Y0;
+	resultX = re.X1;
+	resultY = re.Y1;
+	//以上部分是demo，可以删掉重写
+
+
+	// 决策结束，输出结果
+	Step goStep;
+	goStep.X0 = startX;
+	goStep.Y0 = startY;
+	goStep.X1 = resultX;
+	goStep.Y1 = resultY;
+	cout << "Go" << startX << " " << startY << " " << resultX << " " << resultY << endl;
+	return goStep;
+}
+
+void Wy_ProcStep(int x0, int y0, int x1, int y1, int color)
+{
 	if (abs(x0 - x1) > 1 || abs(y0 - y1) > 1) {
 		//即跳过一个棋子，原来位置棋子消失
 		gridInfo[x0][y0] = 0;
@@ -37,46 +98,15 @@ void ProcStep(int x0, int y0, int x1, int y1, int color)
 		}
 	}
 }
-Step GetNextStep(struct Step requests[], struct Step responses[], int StepCount) {
-	// 初始化棋盘
-	gridInfo[0][0] = gridInfo[6][6] = 1;  //|黑|白|
-	gridInfo[6][0] = gridInfo[0][6] = -1; //|白|黑|
-	//原本这里是读入JSON，这里不需要读入，已知有StepCount个步骤了
-	// 分析自己收到的输入和自己过往的输出，并恢复状态
-	int turnID = StepCount;
-	cout << "Count:" << StepCount << endl;
-	currBotColor = requests[0].X0 < 0 ? 1 : -1; // 第一回合收到坐标是-1, -1，说明我是黑方
-	for (int i = 0; i < turnID; i++)
-	{
-		cout << "requests[" << i <<"](" << requests[i].X0 << "," << 
-			requests[i].Y0 << ")(" << requests[i].X1 << "," << requests[i].Y1  << ")" << endl;
-		if (requests[i].X0 >= 0)
-			ProcStep(requests[i].X0, requests[i].Y0, requests[i].X1,
-				requests[i].Y1, -currBotColor); // 模拟对方落子
-		if (responses[i].X0 >= 0)
-			ProcStep(responses[i].X0, responses[i].Y0, responses[i].X1,
-				responses[i].Y1, currBotColor); // 模拟己方落子%
-	}
-	for (int i = 0; i < 7; i++) {
-		for (int j = 0; j < 7; j++) {
-			if (gridInfo[i][j] == 0) cout << '0';
-			else if (gridInfo[i][j] == 1) cout << '#';
-			else if (gridInfo[i][j] == -1) cout << '.';
-		}
-		cout << endl;
-	}
-	// 找出合法落子点并做出决策
-	int startX, startY, resultX, resultY;
-	//进行决策，得出落子坐标，并赋值给上述变量
-	//在这里添加你的代码
-
+Step Wy_NextStep()
+{
 	vector<Step> DiffCanGo; //找到所有的结果不相同的行走方式
-	//先找到走临近的。这时候应该对所有空位寻找，因为是复制，怎么走都是一样的
+							//先找到走临近的。这时候应该对所有空位寻找，因为是复制，怎么走都是一样的
 	for (int i = 0; i<7; i++)
 	{
 		for (int j = 0; j<7; j++)
 		{
-			if (gridInfo[i, j] == 0)
+			if (gridInfo[i][j] == 0)
 			{
 				//是空的
 				for (int k = -1; k<2; k++)
@@ -130,16 +160,59 @@ Step GetNextStep(struct Step requests[], struct Step responses[], int StepCount)
 			}
 		}
 	}
-	startX = DiffCanGo.at(0).X0;
-	startY = DiffCanGo.at(0).Y0;
-	resultX = DiffCanGo.at(0).X1;
-	resultY = DiffCanGo.at(0).Y1;
-
-	// 决策结束，输出结果
-	Step goStep;
-	goStep.X0 = startX;
-	goStep.Y0 = startY;
-	goStep.X1 = resultX;
-	goStep.Y1 = resultY;
-	return goStep;
+	cout << "Size:" << DiffCanGo.size() << endl;
+	for (int i = 0; i < DiffCanGo.size(); i++) {
+		cout << "Cango:" << DiffCanGo.at(i).X0 << ' ' <<
+			DiffCanGo.at(i).Y0 << ' ' << DiffCanGo.at(i).X1 << ' ' <<
+			DiffCanGo.at(i).Y1 << endl;
+	}
+	int Change = -1;
+	int TargetIndex = 0;
+	for (int index=0; index<DiffCanGo.size(); index++)
+	{
+		if (abs(DiffCanGo.at(index).X0 - DiffCanGo.at(index).X1) == 2 ||
+			abs(DiffCanGo.at(index).Y0 - DiffCanGo.at(index).Y1) == 2)
+		{
+			//是两步
+			int Sum = 0;
+			for (int i = -1; i<2; i++)
+			{
+				for (int j = -1; j<2; j++)
+				{
+					if (!(DiffCanGo.at(index).X1 + i<0 || DiffCanGo.at(index).X1 + i>6 ||
+						DiffCanGo.at(index).Y1 + j<0 || DiffCanGo.at(index).Y1 + j>6)) {
+						if (gridInfo[DiffCanGo.at(index).X1 + i][DiffCanGo.at(index).Y1 + j] ==
+							-currBotColor) Sum++;
+					}
+				}
+			}
+			if (Sum > Change)
+			{
+				TargetIndex = index;
+				Change = Sum;
+			}
+		}
+		else
+		{
+			//走一步
+			int Sum = 1;
+			for (int i = -1; i < 2; i++)
+			{
+				for (int j = -1; j < 2; j++)
+				{
+					if (!(DiffCanGo.at(index).X1 + i<0 || DiffCanGo.at(index).X1 + i>6 ||
+						DiffCanGo.at(index).Y1 + j<0 || DiffCanGo.at(index).Y1 + j>6)) {
+						if (gridInfo[DiffCanGo.at(index).X1 + i][DiffCanGo.at(index).Y1 + j] ==
+							-currBotColor) Sum++;
+					}
+				}
+			}
+			if (Sum > Change)
+			{
+				TargetIndex = index;
+				Change = Sum;
+			}
+		}
+	}
+	return DiffCanGo.at(TargetIndex);
 }
